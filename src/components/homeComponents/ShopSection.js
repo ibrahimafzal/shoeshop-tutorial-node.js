2dda9b389fc19efe010c1f0122ff90840d520b1e
ibrahimafzal <ibrahimtech2015@gmail.com>
2022-08-11 20:02:05 +0500

    first commit

---
+++ src/components/homeComponents/ShopSection.js
@@ -0,0 +1,67 @@
+import React,{useState,useEffect} from "react";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import Pagination from "./pagination";
+// import products from "../../data/Products";
+import axios from "axios"
+import products from "../../../../Server/Data/Products";
+
+const ShopSection = () => {
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchproduct = async() => {
+      const {data} = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    };
+    fetchproduct();
+  },[])
+
+
+  return (
+    <>
+      <div className="container">
+        <div className="section">
+          <div className="row">
+            <div className="col-lg-12 col-md-12 article">
+              <div className="shopcontainer row">
+                {products.map((product) => (
+                  <div
+                    className="shop col-lg-4 col-md-6 col-sm-6"
+                    key={product._id}
+                  >
+                    <div className="border-product">
+                      <Link to={`/products/${product._id}`}>
+                        <div className="shopBack">
+                          <img src={product.image} alt={product.name} />
+                        </div>
+                      </Link>
+
+                      <div className="shoptext">
+                        <p>
+                          <Link to={`/products/${product._id}`}>
+                            {product.name}
+                          </Link>
+                        </p>
+
+                        <Rating
+                          value={product.rating}
+                          text={`${product.numReviews} reviews`}
+                        />
+                        <h3>${product.price}</h3>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+                {/* Pagination */}
+                <Pagination />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ShopSection;
